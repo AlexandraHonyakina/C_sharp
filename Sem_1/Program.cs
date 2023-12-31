383908c42b4e719ffc71d97dce2cad7dc2836a4e
Alexandra Honyakina <ndra@yandex.ru>
2023-12-31 12:59:06 +0400

    Add Sem_1

---
+++ Sem_1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите первое число:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+      
+Console.WriteLine("Введите второе число:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine(firstNumber + " больше, чем " + secondNumber);
+}
+else
+{
+   if (firstNumber < secondNumber)
+{
+    Console.WriteLine(secondNumber + " больше, чем " + firstNumber);
+}
+else
+{
+    Console.WriteLine("Оба числа равны");
+
+    }
+    }
+
